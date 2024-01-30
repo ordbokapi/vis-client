e3df66ab1bc741558b3f8078a2e79e63f037ee13
Adaline Simonian <adalinesimonian@gmail.com>
2024-01-30 01:18:30 -0800

    feat: add background grid to graph view

--- src/rendering/graph-view.ts
+++ src/rendering/graph-view.ts
@@ -26,6 +26,11 @@ export class GraphView extends EventTarget {
   #viewport: Viewport;
 
   /**
+   * Grid graphics.
+   */
+  #gridGraphics: pixi.Graphics;
+
+  /**
    * Pixi application instance.
    */
   #application: pixi.Application;
@@ -74,6 +79,7 @@ export class GraphView extends EventTarget {
     this.#appStateManager = AppStateManager.global.for(this);
 
     this.#overlayCanvas = this.#viewport.addChild(new pixi.Graphics());
+    this.#gridGraphics = this.#viewport.addChild(new pixi.Graphics());
 
     this.#simulation = d3.forceSimulation<d3.SimulationNodeDatum & Article>();
 
@@ -85,6 +91,13 @@ export class GraphView extends EventTarget {
 
     this.#viewport.on('zoomed', () => {
       this.#renderIsEmptyOverlay();
+      this.#renderGrid();
+    });
+
+    this.#appStateManager.observe('translation', () => {
+      window.requestAnimationFrame(() => {
+        this.#renderGrid();
+      });
     });
   }
 
@@ -132,6 +145,7 @@ export class GraphView extends EventTarget {
   render() {
     this.#nodeGraph.render();
     this.#renderIsEmptyOverlay();
+    this.#renderGrid();
   }
 
   #renderIsEmptyOverlay() {
@@ -141,15 +155,6 @@ export class GraphView extends EventTarget {
     if (this.#simulation.nodes().length) {
       return;
     }
-
-    this.#overlayCanvas.beginFill(0x000000, 0.5);
-    this.#overlayCanvas.drawRect(
-      0,
-      0,
-      this.#viewport.width,
-      this.#viewport.height,
-    );
-    this.#overlayCanvas.endFill();
 
     const text = new pixi.Text(
       'Ingen data. Søk etter ein artikkel for å starta.',
@@ -164,5 +169,68 @@ export class GraphView extends EventTarget {
     text.anchor.set(0.5);
     text.position.set(this.#viewport.width / 2, this.#viewport.height / 2);
     this.#overlayCanvas.addChild(text);
+  }
+
+  /**
+   * Render an unobtrusive grid that fills the viewport background.
+   */
+  #renderGrid() {
+    const { x, y, width, height } = this.#viewport.getVisibleBounds();
+
+    const scale = this.#viewport.scale.x;
+    const maxCellSize = 400;
+    let steps: number;
+
+    if (scale < 0.5) {
+      steps = 1; // only 400px grid cells
+    } else if (scale < 1) {
+      steps = 2; // 400px and 200px grid cells
+    } else if (scale < 1.5) {
+      steps = 3; // 400px, 200px and 100px grid cells
+    } else {
+      steps = 4; // 400px, 200px, 100px and 50px grid cells
+    }
+
+    const [colorA, colorB] =
+      steps % 2 ? [0x252525, 0x303030] : [0x303030, 0x252525];
+
+    this.#gridGraphics.clear();
+
+    const minCellSize = maxCellSize / Math.pow(2, steps - 1);
+
+    // use modular math and the current viewport translation to ensure that
+    // the grid seems to move with the viewport
+    const offsetX = x % (minCellSize * 2);
+    const offsetY = y % (minCellSize * 2);
+
+    const startX = x - offsetX - minCellSize * 2;
+    const startY = y - offsetY - minCellSize * 2;
+
+    const endX = x + width + minCellSize;
+    const endY = y + height + minCellSize;
+
+    this.#gridGraphics.lineStyle(1 / scale, colorA, 0.8);
+
+    for (let i = startX; i <= endX; i += minCellSize * 2) {
+      this.#gridGraphics.moveTo(i, startY);
+      this.#gridGraphics.lineTo(i, endY);
+    }
+
+    for (let i = startY; i <= endY; i += minCellSize * 2) {
+      this.#gridGraphics.moveTo(startX, i);
+      this.#gridGraphics.lineTo(endX, i);
+    }
+
+    this.#gridGraphics.lineStyle(1 / scale, colorB, 0.8);
+
+    for (let i = startX + minCellSize; i <= endX; i += minCellSize * 2) {
+      this.#gridGraphics.moveTo(i, startY);
+      this.#gridGraphics.lineTo(i, endY);
+    }
+
+    for (let i = startY + minCellSize; i <= endY; i += minCellSize * 2) {
+      this.#gridGraphics.moveTo(startX, i);
+      this.#gridGraphics.lineTo(endX, i);
+    }
   }
 }
